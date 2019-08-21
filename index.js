79bce07bd6650729c98ab55be24c36fb1f871f46
John Elder <john@codemy.com>
2019-08-21 08:17:06 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+const otherstuff = "hello there, this is other stuff!";
+
+// Set handlebar routes
+app.get('/', function (req, res) {
+    res.render('home', {
+    	stuff: otherstuff
+    });
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
