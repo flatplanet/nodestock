baf9e8eae1a56c9851ab1c6808f00609996641d5
John Elder <john@codemy.com>
2019-08-27 12:52:47 -0700

    pushing to heroku

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 // Stock Market Portfolio App By John Elder Codemy.com
-
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
+
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// API KEY pk_062031d20883444f9ea74e2610fe2011
+// create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_062031d20883444f9ea74e2610fe2011', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
 
 // Set Handlebars Middleware
 app.engine('handlebars', exphbs());
@@ -13,11 +32,34 @@ app.set('view engine', 'handlebars');
 
 const otherstuff = "hello there, this is other stuff!";
 
-// Set handlebar routes
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-    res.render('home', {
-    	stuff: otherstuff
-    });
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+		
+});
+
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+		
+});
+
+
+
+
+// create about page route
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder
